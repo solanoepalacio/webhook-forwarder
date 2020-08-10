78dc6b46abb6badb25847532594c9c14fffa527d
solano <solanoepalacio@gmail.com>
2020-08-10 03:13:22 -0300

    add target server to known hosts on build

--- rproxy/start_tunnel.sh
+++ rproxy/start_tunnel.sh
@@ -7,6 +7,10 @@ echo "FORWARD_TO_HOST: $FORWARD_TO_HOST"
 
 chmod 400 key.pem
 
+mkdir ~/.ssh
+touch ~/.ssh/known_hosts
+ssh-keyscan -H $FORWARD_FROM_HOST >> ~/.ssh/known_hosts
+
 CMD="ssh -i ./key.pem -nN -y -R $FORWARD_FROM_PORT:$FORWARD_TO_HOST:$FORWARD_TO_PORT ec2-user@$FORWARD_FROM_HOST"
 
 echo $CMD
